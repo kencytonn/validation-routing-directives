5ebfd75df93176260883769a82875946e37b534e
kencytonn <kmutua@cytonn.com>
2015-09-22 20:28:10 +0300

    Progres on Routes

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
- var app=angular.module('bookstore',['ngRoute']);
+var app=angular.module('bookstore',['ngRoute']);
 
 app.controller('LoginController', function($scope){
-$scope.user = null; 
-$scope.master = null; 
-$scope.save = function(){ 
-$scope.master = angular.copy($scope.user); 
-}
+	$scope.user = null; 
+	$scope.master = null; 
+	$scope.save = function(){ 
+		$scope.master = angular.copy($scope.user); 
+	}
 });
 // configure our routes
-    app.config(function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl : '/views/login.html',
-                controller  : 'LoginController'
-            })
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl : 'login.html',
+            controller  : 'LoginController'
+        })
 
-            .when('/SignUp', {
-                templateUrl : '/views/singup.html',
-                controller  : 'LoginController'
-            })
-            .otherwise({
-        redirectTo: '/'
-      });
-    });
+        .when('/SignUp', {
+            templateUrl : 'signup.html',
+            controller  : 'LoginController'
+        })
+        .otherwise({
+    redirectTo: '/'
+  });
+});
